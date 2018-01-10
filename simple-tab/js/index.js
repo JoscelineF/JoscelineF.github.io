3fcde7466fbf34338a386cfed94aafd3ead2203d
Josceline Feng <22095436+JoscelineF@users.noreply.github.com>
2018-01-10 22:14:30 +0800

    Update index.js

--- simple-tab/js/index.js
+++ simple-tab/js/index.js
@@ -1,9 +1,15 @@
 window.onload = function(){
   var tabs = document.getElementsByTagName("li");
   var contents = document.getElementById("contents").getElementsByTagName("div");
+  tabs[0].className="activ";
   //console.log(contents);
   for(let i=0; i<tabs.length; i++){
     tabs[i].onmouseover = function(){
+      for(let k=0; k<tabs.length; k++){
+        tabs[k].className='';
+        tabs[i].className="activ";
+      }
+      
       for(let j=0;j<contents.length;j++){
         contents[j].style.display = "none";           contents[i].style.display= "block";         }  
     };
